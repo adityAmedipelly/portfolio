5874a7619fbde153b393a5144a99c4138d0b60c8
adityamedipelly3@gmail.com <adityamedipelly3@gmail.com>
2025-04-09 04:51:21 +0530

    project updated

--- src/Componets/Projects.jsx
+++ src/Componets/Projects.jsx
@@ -1,12 +1,19 @@
 import coder from "../assets/coder.png";
 import SpicyBites from "../assets/SpicyBites.png";
-import Youtube from "../assets/Youtube.png";
+import sensei from "../assets/sensei.png"
 import employee from "../assets/employee.png";
 import Supercar from "../assets/Supercar.png";
 import Cards from "./Cards";
 
 const Projects = () => {
   const projectJson = [
+    {
+      title: "Sensei-Ai",
+      desc: " Built an AI Career Coach with 📄 resume tools, 🎯 quizzes, and 📊 market insights — boosted efficiency ⚡60% & retention 📈35%.",
+      image: sensei,
+      live: "https://sensei-ai-two.vercel.app/",
+      github: "https://github.com/adityAmedipelly/Sensei-Ai",
+    },
     {
       title: "AI Code Reviewer",
       desc: "Build an AI code reviewer with the MERN stack! 🚀 Use AI to analyze, review, and improve code, enhancing development efficiency.",
@@ -15,33 +22,25 @@ const Projects = () => {
       github: "https://github.com/adityAmedipelly/AI-Code-Reviewer",
     },
     {
-      title: "Spicy Bites",
-      desc: "The team at Webelite Builders exceeded our expectations with their digital marketing expertise. Highly recommend!",
-      image: SpicyBites,
-      live: "https://spicybites.netlify.app/",
-      github: "https://github.com/rohitsingh93300/YtSpicyBites",
-    },
-    {
-      title: "YouTube Clone",
-      desc: "Working with Webelite Builders was a seamless experience. They brought our vision to life with a modern, responsive website.",
-      image: Youtube,
-      live: "https://youtube-clone-93300.netlify.app/",
-      github: "https://github.com/rohitsingh93300/YouTube-clone",
-    },
-    {
       title: "Employee Management System",
       desc: "Built an Employee Management System with React JS and Local Storage, allowing admins to assign tasks 📝 and employees to mark them as complete ✅",
       image: employee,
       live: "https://aditec.netlify.app/",
       github: "https://github.com/adityAmedipelly/employee-management-system",
     },
-    
+    {
+      title: "Spicy Bites",
+      desc: "The team at Webelite Builders exceeded our expectations with their digital marketing expertise. Highly recommend!",
+      image: SpicyBites,
+      live: "https://spicybites.netlify.app/",
+      github: "https://github.com/adityAmedipelly",
+    },
     {
       title: "Super Car",
       desc: "Working with Webelite Builders was a seamless experience. They brought our vision to life with a modern, responsive website.",
       image: Supercar,
       live: "https://supercar123.netlify.app/",
-      github: "https://github.com/rohitsingh93300/supercars",
+      github: "https://github.com/adityAmedipelly",
     },
   ];
 
